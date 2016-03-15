f6aaf3d2219eadb3440b6c8ba14915842b649023
Luca D'Antona <luca.dantona@kettydo.com>
2016-03-15 16:04:19 +0100

    First commit

---
+++ TestNav/ViewController.m
@@ -0,0 +1,28 @@
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor colorWithRed:arc4random_uniform(255)/255. green:arc4random_uniform(255)/255. blue:arc4random_uniform(255)/255. alpha:1.];
+    self.navigationController.interactivePopGestureRecognizer.delegate = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:(self.navigationController.viewControllers.count % 2 == 0) animated:animated];
+}
+
+- (IBAction)indietro:(id)sender {
+    if (self.navigationController.viewControllers.count > 1)
+    {
+        [self.navigationController popViewControllerAnimated:YES];
+    }
+}
+
+@end
